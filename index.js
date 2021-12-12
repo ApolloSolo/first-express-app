fa4e84329c1cf20e31d473f5eb1fb77b9f693db0
ApolloSolo <matt1teixeira@gmail.com>
2021-12-12 16:58:59 -0500

    using req key value pairs to access var paths

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const app = express();
 
-app.use(() => {
-    console.log("A request was just made!");
+// app.use((req, res) => {
+//     console.log("A request was just made!");
+//     res.send("Hello, we recieved your request!")
+// });
+
+app.get('/', (req, res) => {
+    res.send("https://google.com");
 });
+
+app.get('/r/:subreddit', (req, res) => {
+    const {subreddit} = req.params;
+    res.send(`<h1>This is the ${subreddit} subreddit.</h1>`);
+})
+
+app.get('/r/:subreddit/:postID', (req, res) => {
+    const {subreddit, postID} = req.params;
+    res.send(`<h1>Viewing ${postID} on the ${subreddit} subreddit.</h1>`);
+})
+
+app.get('/cats', (req, res) => {
+    console.log('Cats page requested');
+    res.send("Meow");
+});
+
+app.get('/dogs', (req, res) => {
+    res.send("BARK");
+});
+
+app.get('*', (req, res) => {
+    res.send("Do not know that request.")
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000")
